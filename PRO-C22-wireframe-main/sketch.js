faaa9b18983ba526f29aac0114a90b6dd802d38b
cpiacitellitieghi <98843278+cpiacitellitieghi@users.noreply.github.com>
2023-05-04 15:48:32 -0300

    Update sketch.js

--- PRO-C22-wireframe-main/sketch.js
+++ PRO-C22-wireframe-main/sketch.js
@@ -2,38 +2,42 @@ const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
-var engine, world,ground;
-var ursinho, spike, fundo, ground;
+var engine, world, groundMatter;
+var ursinho, spike, fundo, groundImg;
 var ursinhoIMG, spikeIMG, fundoIMG, groundIMG;
 var nuvemN, nuvemR, nuvemB;
+var nuvemGroup;
 
 function preload() {
  
-fundoIMG = loadImage("./assets/world1.png");
-ursinhoIMG = loadImage("./assets/bearb1.png");
-groundIMG = loadImage("./assets/ground.png");
-spikeIMG = loadImage("./assets/spike.png");
-nuvemN = loadImage("./assets/nuvemN.png");
-nuvemR = loadImage("./assets/nuvemR.png");
-nuvemB = loadImage("./assets/nuvemB.png");
+  fundoIMG = loadImage("./assets/world1.png");
+  ursinhoIMG = loadImage("./assets/bearb1.png");
+  groundIMG = loadImage("./assets/ground.png");
+  spikeIMG = loadImage("./assets/spike.png");
+  nuvemN = loadImage("./assets/nuvemN.png");
+  nuvemR = loadImage("./assets/nuvemR.png");
+  nuvemB = loadImage("./assets/nuvemB.png");
 
 }
+
 function setup() {
 
   canvas = createCanvas(windowWidth, windowHeight);
   engine = Engine.create();
   world = engine.world;
   
- options={
- isStatic:true
- }
+  var options={
+   isStatic:true
+  };
  
- ground= Bodies.rectangle(0,height-1, width*2,1,options);
- World.add(world,ground);
+  groundMatter = Bodies.rectangle(0,height-1, width*2,1,options);
+  World.add(world,groundMatter);
 
-ursinho=createSprite(height-100,width-300,200,200);
-ursinho.addImage(ursinhoIMG)
-ursinho.scale = 0.8;
+  ursinho=createSprite(height-100,width-300,200,200);
+  ursinho.addImage(ursinhoIMG);
+  ursinho.scale = 0.8;
+
+  nuvemGroup = new Group();
 
 }
 
@@ -41,17 +45,16 @@ function draw() {
   background(fundoIMG);
   Engine.update(engine);
  
- rect(ground.position.x, ground.position.y,width*2,1);
- drawSprites() 
- spawnclouds()
+  rect(groundMatter.position.x, groundMatter.position.y,width*2,1);
+  drawSprites(); 
+  spawnclouds();
  
 }
+
 function spawnclouds() {
   if(frameCount % 60 === 0) {
     var nuvem = createSprite(width+100,height+300,10,40);
-    //obstacle.debug = true;
     
-    //gere obstáculos aleatórios
     var rand = Math.round(random(1,6));
     switch(rand) {
       case 1: nuvem.addImage(nuvemN);
@@ -64,10 +67,10 @@ function spawnclouds() {
       default: break;
     }
     
-    //atribua dimensão e tempo de vida ao obstáculo           
     nuvem.scale = 1;
     nuvem.lifetime = 300;
-    //adicione cada obstáculo ao grupo
+    
     nuvemGroup.add(nuvem);
   }
 }
+
